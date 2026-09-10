863efddc7e1173f59dfd572d33e56debbdc8e45c
Khan Tufail Wajiha (2025) ZPAC136 <wajiha.khantufail.2025@live.rhul.ac.uk>
2026-09-10 03:48:28 +0100

    Add command-line log file argument

--- Projects/security-log-analyser/analyser.py
+++ Projects/security-log-analyser/analyser.py
@@ -1,3 +1,9 @@
+import argparse #so program can recieve argument from terminal
+
+parser = argparse.ArgumentParser()
+parser.add_argument("logfile")
+args = parser.parse_args()
+
 def count_failed_logins(file):
     failed_attempts = {}
 
@@ -15,17 +21,19 @@ def count_failed_logins(file):
                     
     return(failed_attempts)
 
-with open("auth.log","r") as file:
-    result = count_failed_logins(file)
+def main():
+    try:
+        with open(args.logfile,"r") as file:
+            result = count_failed_logins(file)
+
+    except FileNotFoundError:
+        print("Log file not found")
+        return #means leave main() function and stop the rest of the program
 
     for ip, count in result.items():
         if count >= 3:
             print(f"IP: {ip} - Failed attempts: {count}")
 
-try:
-    with open("auth.log","r") as file:
-        result = count_failed_logins(file)
 
-except FileNotFoundError:
-    print("Log file not found")
+main()
 
